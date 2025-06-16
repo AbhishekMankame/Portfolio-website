e19c366aa3422ed331070c5d052269fdf7950fdd
abhishek <abhishekmankame962@gmail.com>
2025-06-17 01:43:40 +0530

    simple code update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const app = () => {
     return (
-        <div>app</div>
+        <div><h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1></div>
     )
 }
 
