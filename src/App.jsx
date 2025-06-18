3b3d000fe086ad0af8687b1bb9b09e9c2fbea22d
abhishek <abhishekmankame962@gmail.com>
2025-06-18 22:49:35 +0530

    simple update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import { Router, BrowserRoute as Router, Routes } from 'react-router-dom';
 
 const app = () => {
     return (
-        <div><h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1></div>
+        <main className="bg-slate-300/20">
+            <Router>
+                <Navigate></Navigate>
+
+            </Router>
+
+
+        </main>
     )
 }
 
